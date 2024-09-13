e6a492847bb1ecd3f767c2812ac637252e1be011
semaphoresentinel <149366454+semaphoresentinel@users.noreply.github.com>
2024-09-13 19:00:06 +0800

    fix: project code

--- api/request.ts
+++ api/request.ts
@@ -3,7 +3,7 @@ import request from './axios';
 
 export const reportAccount = async (body: { caHash: string, operationType: string }): Promise<void> => {
   console.log('exe reportAccount!')
-  return request.post('/app/report/account', {clientType: 'TgBot', projectCode: '1000', ...body});
+  return request.post('/app/report/account', {clientType: 'TgBot', projectCode: '10000', ...body});
 };
 // export const setCurUsingBeanPass = async (body: ISetCurBeanBody): Promise<IBeanPassListItem> => {
 //   return request.post('/app/bean-pass/using', body);
